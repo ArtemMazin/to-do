8c511ae6a10052cb6f5c46630a196ef926f50865
ArtemMazin <minima1s@yandex.ru>
2023-02-23 15:35:23 +0300

    feat: добавить закрытие попапа по кнопке

--- script/todo.js
+++ script/todo.js
@@ -7,12 +7,18 @@ const tasksList = document.querySelector('.tasks__list'),
   inputText = document.querySelector('.popup__input_type_text'),
   popup = document.querySelector('.popup-container'),
   openPopupButton = document.querySelector('.task__button'),
+  closePopupButton = document.querySelector('.popup__close-button'),
   submitButton = document.querySelector('.popup__button');
 
 function openPopup() {
   popup.classList.add('popup-container_active');
 }
 openPopupButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup-container_active');
+}
+closePopupButton.addEventListener('click', closePopup);
 
 const formValidation = new FormValidator(settingsValidation);
 formValidation.enableValidation();
